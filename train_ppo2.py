6ac116a20350a17fc9e57d726bc05abb85d41430
Andriy Drozdyuk <andriy@drozdyuk.com>
2019-06-11 16:31:31 -0400

    Got rid of parallelized because graph didn't look right

--- train_ppo2.py
+++ train_ppo2.py
@@ -1,22 +1,22 @@
 import gym
 import subworldgym
 from stable_baselines.common.policies import MlpPolicy
-from stable_baselines.common.vec_env import SubprocVecEnv
+from stable_baselines.common.vec_env import DummyVecEnv
 from stable_baselines import PPO2
 import os.path
 
-n_cpu = 4
-env = SubprocVecEnv([lambda: gym.make('SubWorld-v0') for i in range(n_cpu)])
+env = DummyVecEnv([lambda: gym.make('SubWorld-v0')])
 
-
-# if os.path.isfile('submarine.pkl'):
-# 	model = PPO2.load('submarine_parallel', env=env, verbose=1, tensorboard_log="./ppo_parallel_tensorboard/")
-# else:
-model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log="./ppo_tensorboard/")
+if os.path.isfile('submarine_ppo.pkl'):
+	model = PPO2.load('submarine_ppo', env=env, verbose=1, tensorboard_log="./ppo_tensorboard/")
+else:
+	model = PPO2(MlpPolicy, env, verbose=1, tensorboard_log="./ppo_tensorboard/")
 
 print("Training model.")
-model.learn(total_timesteps=10000)
 
-print("Saving the model.")
-model.save("submarine_ppo")
+# Save model periodically
+for i in range(1000):
+	model.learn(total_timesteps=1000)
+	model.save("submarine_ppo")
+
 env.close()
